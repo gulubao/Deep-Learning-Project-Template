635dda32673ed609220196631a9c904a03a4694b
gulubao <1047503667@qq.com>
2024-04-16 15:23:47 -0400

    update shell

--- train.sh
+++ train.sh
@@ -1,3 +1,9 @@
+"""
+cd /home/gulu/code/research/human_adaption_predict
+conda activate human_behaviour_prediction
+free -m && sudo sh -c 'sync && echo 3 > /proc/sys/vm/drop_caches' && free -m
+"""
+
 python tools/train_net.py \
     --config-file configs/COCO-Detection/faster_rcnn_R_50_FPN_1x.yaml \
     OUTPUT_DIR /content/drive/MyDrive/Colab_Notebooks/CenterNet/CenterNet-CondInst/CenterNet-CondInst-Output
