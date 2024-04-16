2f0e51f1aa5f97664989234f0e383496b1ae724d
gulubao <1047503667@qq.com>
2024-04-16 15:26:17 -0400

    update debugger

--- tools/train_net.py
+++ tools/train_net.py
@@ -81,4 +81,13 @@ def main():
 
 
 if __name__ == '__main__':
+    import debugpy
+    try:
+        # 5678 is the default attach port in the VS Code debug configurations. Unless a host and port are specified, host defaults to 127.0.0.1
+        debugpy.listen(("localhost", 9501))
+        print("Waiting for debugger attach")
+        debugpy.wait_for_client()
+    except Exception as e:
+        pass
+    
     main()
