9992001c63ecab35a185849f3cdbd43f7126f9e4
gulubao <1047503667@qq.com>
2024-08-01 09:44:55 -0400

    chore: Remove __pycache__ directories in train.sh script

--- train.sh
+++ train.sh
@@ -2,6 +2,13 @@
 cd /home/gulu/code/research/human_adaption_predict
 conda activate human_behaviour_prediction
 free -m && sudo sh -c 'sync && echo 3 > /proc/sys/vm/drop_caches' && free -m
+
+
+# -- remove cache --#
+BASE_DIR="~/code/reproduce/Deep-Learning-Project-Template"
+find $BASE_DIR -type d -name "__pycache__" -exec rm -r {} + # Find and remove all __pycache__ directories
+echo "All __pycache__ directories under $BASE_DIR have been removed."
+
 """
 
 python tools/train_net.py \
